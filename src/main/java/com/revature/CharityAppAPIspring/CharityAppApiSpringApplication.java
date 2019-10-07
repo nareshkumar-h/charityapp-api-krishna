5adbf8fdcc49be07ec0f606d188a248e353c884a
krishnawisely <krishna192168@gmail.com>
2019-10-06 20:57:05 -0700

    db connection

---
+++ src/main/java/com/revature/CharityAppAPIspring/CharityAppApiSpringApplication.java
@@ -0,0 +1,13 @@
+package com.revature.CharityAppAPIspring;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CharityAppApiSpringApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CharityAppApiSpringApplication.class, args);
+	}
+
+}
